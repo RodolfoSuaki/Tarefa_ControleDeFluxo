6954effcf8d5f2dbcabc36a268f2ca54d5b2488b
Rodolfo Suaki <rmsuaki@hotmail.com>
2024-01-01 22:43:17 -0300

    Inserindo código

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,50 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
+
+    static float nota1;
+    static float nota2;
+    static float nota3;
+    static float nota4;
+
+    static float media;
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        leituraDeNotas();
+        mediaNotas();
+        System.out.println("\nA sua média é: "+mediaNotas());
+        verificaStatusDoAluno(mediaNotas());
+        System.out.println(verificaStatusDoAluno(mediaNotas()));
+    }
+    private static String verificaStatusDoAluno(float media) {
+        if (media >= 7){
+            return "Você está aprovado";
+        }else if (media >= 5){
+            return "Você está de recuperação";
+        }else{
+            return "Você está reprovado";
         }
     }
-}
+
+    public static void leituraDeNotas(){
+        Scanner num1 = new Scanner (System.in);
+        System.out.println("Digite sua nota 1: ");
+        nota1 = num1.nextFloat();
+
+        Scanner num2 = new Scanner (System.in);
+        System.out.println("Digite sua nota 2: ");
+        nota2 = num2.nextFloat();
+
+        Scanner num3 = new Scanner (System.in);
+        System.out.println("Digite sua nota 3: ");
+        nota3 = num3.nextFloat();
+
+        Scanner num4 = new Scanner (System.in);
+        System.out.println("Digite sua nota 4: ");
+        nota4 = num4.nextFloat();
+    }
+    public static float mediaNotas(){
+        float media = (nota1 + nota2 + nota3 + nota4) / 4;
+        return media;
+    }
+    }
